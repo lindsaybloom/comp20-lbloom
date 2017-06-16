78830efd3c443680f10279cc761da82efc33a765
Lindsay Bloom <lindsay.bloom@tufts.edu>
2017-06-15 21:42:43 -0400

    added XMLHTTP request and messages appear

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,20 @@
-// Your JavaScript goes here...
+var xmlhttp = new XMLHttpRequest();
+var url = "data.json";
+
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var messages = JSON.parse(this.responseText);
+        parse(messages);
+    }
+};
+
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
+function parse(jsonData) {
+    var out = "";
+    var i;
+    var boy = jsonData[0];
+    var george = jsonData[1];
+    document.getElementById("messages").innerHTML = (boy.content + " " + boy.username + "<br>" + george.content + " " + george.username);
+}
