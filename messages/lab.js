d8efde53179c0db10f272cb7443ce1114b30c154
Lindsay Bloom <lindsay.bloom@tufts.edu>
2017-06-15 23:39:37 -0400

    fixed console error

--- messages/lab.js
+++ messages/lab.js
@@ -1,20 +1,19 @@
-var xmlhttp = new XMLHttpRequest();
-var url = "data.json";
+function parse() {
+    var xmlhttp = new XMLHttpRequest();
+    var url = "data.json";
 
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var messages = JSON.parse(this.responseText);
-        parse(messages);
-    }
-};
+    xmlhttp.open("GET", url, true);
+    
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var messages = JSON.parse(this.responseText);
+            var out = "";
+            var i;
+            var boy = messages[0];
+            var george = messages[1];
+            document.getElementById("messages").innerHTML = (boy.content + " " + boy.username + "<br>" + george.content + " " + george.username);
+        }
+    };
 
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
-
-function parse(jsonData) {
-    var out = "";
-    var i;
-    var boy = jsonData[0];
-    var george = jsonData[1];
-    document.getElementById("messages").innerHTML = (boy.content + " " + boy.username + "<br>" + george.content + " " + george.username);
+    xmlhttp.send();
 }
